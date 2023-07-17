cb1b02321ea345dd765aea2434b8da226e8cdab6
Mohammed <mohatech777@gmail.com>
2023-07-17 12:33:06 +0300

    initialization commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
