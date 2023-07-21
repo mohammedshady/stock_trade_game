64f19b6d394f6d6643f6c4662e8fd5228d480c40
Mohammed Shady Elsayed <75118102+mohammedshady@users.noreply.github.com>
2023-07-21 18:48:23 +0300

    DB interactions & user related functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 //to be un uncommented
-//const userRoute = require("./routes/user");
+const userRoute = require("./routes/user");
 
 const { initializeCosmosDB } = require("./db");
 const port = 3000;
@@ -10,7 +10,7 @@ app.use(express.json());
 initializeCosmosDB();
 
 //to be uncommented
-//app.use("/api/user", userRoute);
+app.use("/api/user", userRoute);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
