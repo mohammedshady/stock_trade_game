bad43a7466f9d9c06c4c2e4787a0e60743723c9b
Mohammed Shady Elsayed <75118102+mohammedshady@users.noreply.github.com>
2023-07-21 19:02:17 +0300

    modified user routes

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,24 @@
 const router = require("express").Router();
 //const { v4: uuidv4 } = require('uuid');
-const { customAlphabet } = require('nanoid'); //package for unique ID generation
-const generateNumericID = customAlphabet('1234567890', 4); //ID's are of length 4 digits
+//const { customAlphabet } = require('nanoid'); //package for unique ID generation
+//const generateNumericID = customAlphabet('1234567890', 4); //ID's are of length 4 digits
 
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  getUserStocksById,
+  getStockPriceFromAllStocks,
+  updateUserInfo,
+} = require("../db");
 
-
-// stock object
-// key (ex: MSFT) linked with database of stocks
-// num_shares => number of shares a user posses of a certain stock
-
-let users = []; 
-
+const stockKeyMap = {
+  MSFT: "Stock_Data_MSFT",
+  IBM: "Stock_Data_IBM",
+  TSLA: "Stock_Data_TSLA",
+  META: "Stock_Data_META",
+  AAPL: "Stock_Data_JPM",
+};
 
 ////////////////////////////////////////////////////////
 //                                                    //
@@ -19,46 +27,41 @@ let users = [];
 ////////////////////////////////////////////////////////
 
 //returning list of users
-router.get("/users", (req,res) => 
-    {
-        res.json(users);
-    }
-);
-
+router.get("/users", async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    res.status(201).json({ message: "Fetched users List", users });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Error fetching users" });
+  }
+});
 
 //getting a certain player based on the ID (unique identifier)
-router.get("/users/:id", (req, res) => 
-{
+router.get("/users/:id", async (req, res) => {
   // Extract the player ID from the request parameter
-  const id = parseInt(req.params.id);
-  //const {id} = req.params; // -> destructure id from params
+  const { id } = req.params; // -> destructure id from params
   // Find the player with the corresponding ID in the users array
-  const player = users.find(user => user.id === id);
+  const player = await getUserById(id.toString());
   if (player) {
     res.status(200).json(player);
   } else {
-    res.status(404).json({ message: 'Player not found' });
+    res.status(404).json({ message: "Player not found" });
   }
 });
-
 
 //get stocks of a certain player (based on ID)
-router.get("/users/player/:id/stocks", (req, res) => {
+router.get("/users/player/:id/stocks", async (req, res) => {
   // Extract the player ID from the request parameter
-  const id = parseInt(req.params.id);
-  //const {id} = req.params; // -> destructure id from params
+  const { id } = req.params; // -> destructure id from params
   // Find the player with the corresponding ID in the users array
-  const player = users.find(user => user.id === id);
-  if (player) {
-    res.status(200).json(player.stocks);
+  const stocks = await getUserStocksById(id.toString());
+  if (stocks) {
+    res.status(200).json(stocks);
   } else {
-    res.status(404).json({ message: 'Player not found' });
+    res.status(404).json({ message: "Player not found" });
   }
 });
-
-
-module.exports = router;
-
 
 ////////////////////////////////////////////////////////
 //                                                    //
@@ -67,22 +70,29 @@ module.exports = router;
 ////////////////////////////////////////////////////////
 
 //adding a new player
-router.post("/users", (req, res) => {
-  const player = {
-    id: generateNumericID(),
-    name: req.body.name, 
-    money: 5000, // fixed with a constant amount at start of game 
-    //stocks array; initially user has 0 shares 
-    stocks: [ {key:"MSFT", num_shares : 0},
-              {key:"IBM" , num_shares : 0},
-              {key:"TSLA", num_shares : 0},
-              {key:"META", num_shares : 0},
-              {key:"AAPL", num_shares : 0}],  
-  };
-  users.push(player);
-  res.status(201).json({ message: 'User created successfully', users });
+router.post("/users", async (req, res) => {
+  try {
+    const player = {
+      id: req.body.id,
+      name: req.body.name,
+      money: 5000,
+      stocks: [
+        { key: "MSFT", num_shares: 0 },
+        { key: "IBM", num_shares: 0 },
+        { key: "TSLA", num_shares: 0 },
+        { key: "META", num_shares: 0 },
+        { key: "AAPL", num_shares: 0 },
+      ],
+    };
+    // Save the new user to the database (using the createUser function)
+    const user = await createUser(player);
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    // Handle any errors that occurred during user creation
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Error creating user" });
+  }
 });
-
 
 ////////////////////////////////////////////////////////
 //                                                    //
@@ -91,87 +101,77 @@ router.post("/users", (req, res) => {
 ////////////////////////////////////////////////////////
 
 // PUT route for SELLING a stock
-router.put("/users/sell-stock", (req, res) => {
-  const { userId, stockKey, numShares } = req.body;
+router.put("/users/sell-stock", async (req, res) => {
+  const { userId, stockKey, numShares, day } = req.body;
   // Find the user based on the provided userId
-  const player = users.find(user => user.id === userId);
+  const player = await getUserById(userId.toString());
   // player not found
   if (!player) {
-    return res.status(404).json({ error: 'User not found' });
+    return res.status(404).json({ error: "User not found" });
   }
   // Find the stock in the user's stocks array based on the provided stockKey
-  const stockToSell = player.stocks.find(stock => stock.key === stockKey);
+  const stockToSell = player.stocks.find((stock) => stock.key === stockKey);
   // extra validation but not necessary as the player has a relation with all stocks
   if (!stockToSell) {
-    return res.status(404).json({ error: 'Stock not found for this user' });
+    return res.status(404).json({ error: "Stock not found for this user" });
   }
   // Check if the user has enough shares to sell
   if (numShares > stockToSell.num_shares) {
-    return res.status(400).json({ error: 'Insufficient shares to sell' });
+    return res.status(400).json({ error: "Insufficient shares to sell" });
   }
+
   // Calculate the amount to be added to the user's money and the new number of shares
   // Assuming you have a function to get the stock price from your AllStocks database
-  const stockPrice = getStockPriceFromAllStocks(stockKey); 
+  const stockPrice = await getStockPriceFromAllStocks(
+    stockKeyMap[stockKey],
+    day
+  );
   const amountToBeAdded = stockPrice * numShares;
   // Update the user's money and num_shares of the stock
-  player.money += amountToBeAdded;
-  stockToSell.num_shares -= numShares;
+  player.money += parseFloat(amountToBeAdded);
+  stockToSell.num_shares -= parseInt(numShares);
   // Send a success response
-  res.json({ message: 'Stock sold successfully', player });
+  const updatedInfo = await updateUserInfo(userId, player);
+  res.json({ message: "Stock sold successfully", updatedInfo });
 });
 
 // PUT route for BUYING a stock
-router.put("/users/buy-stock", (req, res) => {
-  const { userId, stockKey, numShares } = req.body;
+router.put("/users/buy-stock", async (req, res) => {
+  const { userId, stockKey, numShares, day } = req.body;
   // Find the user based on the provided userId
-  const player = users.find(user => user.id === userId);
+  const player = await getUserById(userId.toString());
   // player not found
   if (!player) {
-    return res.status(404).json({ error: 'User not found' });
+    return res.status(404).json({ error: "User not found" });
   }
   // Find the stock in the user's stocks array based on the provided stockKey
-  const stockToSell = player.stocks.find(stock => stock.key === stockKey);
+  const stockToSell = player.stocks.find((stock) => stock.key === stockKey);
   // extra validation but not necessary as the player has a relation with all stocks
   if (!stockToSell) {
-    return res.status(404).json({ error: 'Stock not found for this user' });
+    return res.status(404).json({ error: "Stock not found for this user" });
   }
   // Calculate the amount to be deducted from the user's money and the new number of shares
   // Assuming you have a function to get the stock price from your AllStocks database
-  const stockPrice = getStockPriceFromAllStocks(stockKey); 
+  const stockPrice = await getStockPriceFromAllStocks(
+    stockKeyMap[stockKey],
+    day
+  );
+
   const amountToBeDeducted = stockPrice * numShares;
   // Check if the user has enough money to buy
   if (amountToBeDeducted > player.money) {
-    return res.status(400).json({ error:'Insufficient money to buy'}).json({message:'your money amount is: '},player.money);
+    return res
+      .status(400)
+      .json({ error: "Insufficient money to buy" })
+      .json({ message: "your money amount is: " }, player.money);
   }
   // Update the user's money and num_shares of the stock
-  player.money -= amountToBeDeducted;
-  stockToSell.num_shares += numShares;
+  player.money -= parseFloat(amountToBeDeducted);
+  stockToSell.num_shares += parseInt(numShares);
   // Send a success response
-  res.json({ message: 'Stock bought successfully', player });
+
+  const updatedInfo = await updateUserInfo(userId, player);
+  res.json({ message: "Stock bought successfully", updatedInfo });
 });
 
-
-
-
-
-////////////////////// TESTING FUNCTION //////////////////////////
-
-// Example function to fetch the stock price from AllStocks database
-// this is only used for testing purposes 
-// to be removed later
-function getStockPriceFromAllStocks(stockKey) {
-  // Replace this with your logic to get the stock price from your database
-  // For simplicity and testing, assume we have a fixed stock price for each stock
-  const stockPrices = {
-    MSFT:  150,
-    AAPL:  200,
-    TSLA:  800,
-    IBM : 2500,
-    META: 1000,
-    // Add other stock prices as needed
-  };
-
-  return stockPrices[stockKey]; 
-  // Return (??) if the stock key is not found (handle edge cases)
-}
-
+module.exports = router;
