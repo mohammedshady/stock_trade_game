c7926716ed7944441df913bc8da503ae9871a37c
Mohammed Shady Elsayed <75118102+mohammedshady@users.noreply.github.com>
2023-07-27 13:05:20 +0300

    remove Route folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRoute = require("./routes/user");
 const gameRoute = require("./routes/game");
 const { initializeCosmosDB } = require("./db");
-
+//route folder remove
 const app = express();
 const port = 3000;
 
