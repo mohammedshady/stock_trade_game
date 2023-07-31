b9701ebfc3430285d07d1bff2f412e5b43cfed64
Amira Abdelghany <amira.say.yes@gmail.com>
2023-07-31 11:22:12 +0200

    fixing stocks.js hoping it works

--- client/src/stocks.js
+++ client/src/stocks.js
@@ -1,31 +1,44 @@
 import React from "react";
 import StockChange from "./StockChange.js";
 
-/* {stockbyday.key.differenceBetweenDayBefore}  */
-const Stocks = ({ ownedstocks }) => {
+ {/* I hope this would work */}
+const Stocks = ({ ownedstocks, stocksdata }) => {
   return (
     <div className="stocks-container">
       <div className="Owned">Owned</div>
-      {ownedstocks.map((stock, index) => (
-        <div className="main" key={index}>
-          <div className="stock-row">
-            <div>
-              <p className="stock-key">{stock.key}</p>
-              <p className="stock-name">name</p>
-            </div>
-            <StockChange percentage={9.44} />
-            <p className="stock-price">$price</p>
-            <div className="amount-owned">
-              <div className="word-container">
-                <span className="word">Amount</span>
-                <span className="word">Owned</span>
+      {ownedstocks.map((stock, index) => {
+        // Find the stock data from stocksdata array based on the stock key
+        const stockData = stocksdata.find((data) => data[stock.key]);
+
+        if (!stockData) {
+          // Handle the case when stock data is not found
+          return null;
+        }
+
+        const { price, difference } = stockData[stock.key];
+
+        return (
+          <div className="main" key={index}>
+            <div className="stock-row">
+              <div>
+                <p className="stock-key">{stock.key}</p>
+                {/* name is to be added when I figure out where is it written */}
+                <p className="stock-name">name</p>
               </div>
-              <p className="stock-amount">{stock.price * stock.num_shares}</p>
+              <StockChange percentage={difference} />
+              <p className="stock-price">${price}</p>
+              <div className="amount-owned">
+                <div className="word-container">
+                  <span className="word">Amount</span>
+                  <span className="word">Owned</span>
+                </div>
+                <p className="stock-amount">{stock.price * stock.num_shares}</p>
+              </div>
             </div>
+            <p className="line"></p>
           </div>
-          <p className="line"></p>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
