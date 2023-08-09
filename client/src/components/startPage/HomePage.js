e5ace14efa515c5a375ea60d0c305de00a7cd82f
FIRST_NAME LAST_NAME <marioayman2017@gmail.com>
2023-08-09 15:10:05 +0300

    changed some things in home and PreGame

--- client/src/components/startPage/HomePage.js
+++ client/src/components/startPage/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./HomePage.css";
 import wolf from "./wolf.jpg";
 import axios from "axios";
@@ -7,11 +7,10 @@ import { useUser } from "../context/SetUser";
 
 function HomePage() {
   const navigate = useNavigate();
-  const { user, updateUser } = useUser();
+  const { updateUser } = useUser();
   const [name, setName] = useState("");
   const [error, setError] = useState("");
 
-  console.log(user);
   const postUserToServer = async () => {
     try {
       const response = await axios.post(
@@ -31,27 +30,24 @@ function HomePage() {
   const createGame = async () => {
     try {
       const response = await axios.post("http://localhost:3000/api/game/game");
-      console.log("Game Created Successfully!");
+      console.log("Username posted successfully!");
 
       //redirect to the game settings page
       return response.data;
     } catch (error) {
-      console.error("Error while posting Game:", error.message);
-      setError("error while posting Game.");
+      console.error("Error while posting username:", error.message);
+      setError("error while posting username.");
     }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    if (name.length === 0) {
+      setError("please enter a name");
+      return;
+    }
     try {
-      if (!user.id) {
-        if (name.length === 0) {
-          setError("please enter a name");
-          return;
-        }
-        await postUserToServer();
-      }
+      await postUserToServer();
       const gameData = await createGame();
       navigate("/pre-game", { state: { gameData } });
     } catch (error) {
@@ -66,45 +62,48 @@ function HomePage() {
   return (
     <form className="App-header" onSubmit={handleSubmit}>
       <div className="page">
+<div className="upperScreen">
         <div className="title">
           <text>The bugs of wall street</text>
         </div>
+        
         <div className="container">
           <div className="left">
             <div className="login">
+              <div className='login-image'>
               <img src={wolf} />
+              </div>
+              <br/>
+              <div className='login-text'>
               <text>
                 By logging in you agree to the ridiculously long terms that you
                 didn't bother to read
               </text>
+              </div>
             </div>
           </div>
           <div className="right">
             <div className="form">
               <label htmlFor="name">Name:</label>
-              {user?.name ? (
-                <p>
-                  Logged in as : <span>{user.name}</span>
-                </p>
-              ) : (
-                <input
-                  type="text"
-                  id="email"
-                  onChange={handleNameChange}
-                  value={name}
-                />
-              )}
-
+              <input
+                type="text"
+                id="email"
+                onChange={handleNameChange}
+                value={name}
+              />
               {error ? <p className="name-error">{error}</p> : null}
             </div>
           </div>
         </div>
-      </div>
-      <div className="App">
+ </div>   
+       <div className="App">
         <button className="button" type="submit">
           START GAME
         </button>
       </div>
+      
+      </div>
+      
     </form>
   );
 }
